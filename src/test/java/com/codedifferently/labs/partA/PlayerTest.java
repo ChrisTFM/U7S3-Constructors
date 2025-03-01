ebd7c302664aaa2ce88e70bd6cb037f3b81a0087
Christian <dynexanimations21@gmail.com>
2025-03-01 12:25:54 -0500

    1
    1

--- src/test/java/com/codedifferently/labs/partA/PlayerTest.java
+++ src/test/java/com/codedifferently/labs/partA/PlayerTest.java
@@ -1,5 +1,14 @@
 package com.codedifferently.labs.partA;
 
+
+import partA.Player;
+
 public class PlayerTest {
+    public static void main(String[] args){
+        Player newPlayer = new Player();
+        System.out.println(newPlayer.name);
+        System.out.println(newPlayer.height);
+        System.out.println(newPlayer.yearsInLeague);
+    }
 
 }
